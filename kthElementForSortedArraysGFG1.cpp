d8a4b93bf9f98b838b568027d6a0527bb5a4fa43
soumak09 <soumak2016@gmail.com>
2022-01-21 21:18:30 +0530

    Add files via upload

---
+++ kthElementForSortedArraysGFG1.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int a[], int b[], int n, int m, int k)
+    {
+        int low = min(a[0],b[0]);
+        int high = max(a[n-1],b[m-1]);
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int x = upper_bound(a,a+n,mid)-a;
+            int y = upper_bound(b,b+m,mid)-b;
+            if(x+y<k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+};
+
+// { Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
